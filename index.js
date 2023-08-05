90c6869af0fd0555af32ff9f25479bd4a3d812dc
Anwesh <anweshkondadi@gmail.com>
2023-08-05 16:29:35 +0530

    bad sum of elements

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * find sum of elements
+ */
+
+function badSumElements(arr) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+      total += arr[i];
+    }
+    return total;
+  }
